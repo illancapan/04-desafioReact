a7f6492df242bb56acfa6c2b02e2937072c2d8a3
illancapan <igorllancapan@gmail.com>
2024-03-22 11:52:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import MiApi from "./components/api/MiApi";
+function App() {
+  return (
+    <>
+      <MiApi />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/api/MiApi.jsx
@@ -0,0 +1,9 @@
+const MiApi = () => {
+  return (
+    <>
+      <h1 className="bg-primary">Limpiando </h1>
+    </>
+  );
+};
+
+export default MiApi;
